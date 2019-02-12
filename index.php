3107671447ed9176bc396ae06e371256735784b1
ComandPromt <vayase_a_ferrol@hotmail.com>
2019-02-12 14:02:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -3,90 +3,87 @@ date_default_timezone_set('Europe/Madrid');
 include("cabecera.html");
 include("funciones.php");
 
-if(login()){
-$conexion=conectarbd("qabe761.iesluisvelez.org","qabe761","599839RMgc","qabe761");
+if (login()) {
+	$conexion = conectarbd("qabe761.iesluisvelez.org", "qabe761", "599839RMgc", "qabe761");
 
-if(gettype($conexion)!="object"){
-	
-	print "<br/><br/><h1>Error al conectar a la base de datos</h1>";
-}
-else{
-	if(isset($_GET["profesor"])&& isset($_GET["profesor"]).trim()!=""){
-		
-		$consulta="SELECT periodo,dia,hora,grupo,materia,observaciones FROM HorariosProfesores WHERE idProfesor=(SELECT idProfesor FROM Profesores WHERE nombreProfesor='".$_GET["profesor"]."')";
-	verconsulta($conexion,$consulta,$_GET["profesor"]);
-	}
-	else{
-		$hora=date("H");
-		$minutos=date("i");
-	
+	if (gettype($conexion) != "object") {
+
+		print "<br/><br/><h1>Error al conectar a la base de datos</h1>";
+	} else {
+		$hora = date("H");
+		$minutos = date("i");
+
 		print '<div name="margen_2" style="width:100%;float:left;height:20%;">
 		<br/>
 			<select style="font-size:28px;" id="producto" onchange="ShowSelected();" >';
-			$hora_actual=saber_hora($hora,$minutos);
-			$horas=array();
-			$horas[]="8:15 - 9:15";
-			$horas[]="9:15 - 10:15";
-			$horas[]="10:15 - 11:15";
-			$horas[]="11:45 - 12:45";
-			$horas[]="12:45 - 13:45";
-			$horas[]="13:45 - 14:45";
-			$y=0;
-	
-			for($x=1;$x<=6;$x++){
-				if($x!=$hora_actual){
-					print '<option value="'.$x.'">'.$horas[$y].'</option>';
-				}
-				else{
-					print '<option value="'.$hora_actual.'">Hora actual</option>';
-				}
-				$y++;
+		$hora_actual = saber_hora($hora, $minutos);
+		$horas = array();
+		$horas[] = "8:15 - 9:15";
+		$horas[] = "9:15 - 10:15";
+		$horas[] = "10:15 - 11:15";
+		$horas[] = "11:45 - 12:45";
+		$horas[] = "12:45 - 13:45";
+		$horas[] = "13:45 - 14:45";
+		$y = 0;
+
+		for ($x = 1; $x <= 6; $x++) {
+			if ($x != $hora_actual) {
+				print '<option value="' . $x . '">' . $horas[$y] . '</option>';
+			} else {
+				print '<option value="' . $hora_actual . '">Hora actual</option>';
 			}
-			print '</select>';
-}			
-			}
+			$y++;
+		}
+		print '</select>';
+
+		if (isset($_GET["profesor"]) && isset($_GET["profesor"]) . trim() != "") {
+
+			$consulta = "SELECT periodo,dia,hora,grupo,materia,observaciones FROM HorariosProfesores WHERE idProfesor=(SELECT idProfesor FROM Profesores WHERE nombreProfesor='" . $_GET["profesor"] . "')";
+			verconsulta($conexion, $consulta, $_GET["profesor"]);
+		}
+
+	}
 
 
-		print '<script>function ShowSelected(){
+	print '<script>function ShowSelected(){
 		var combo = document.getElementById("producto");
 		var selected = combo.options[combo.selectedIndex].value;';
-		print "
+	print "
 		document.cookie ='selected='+selected;
 		document.location.reload();
 		}
 		</script>";
-	
-	if($_COOKIE["selected"]>=1 && $_COOKIE["selected"]<=6){
-		
-		$consulta='SELECT nombreProfesor,diaSemana,hora FROM vAusencias where hora='.$_COOKIE["selected"];
 
-	verconsulta($conexion,$consulta);
-	}
-else{
-	
-$hora_actual=saber_hora($hora,$minutos);
+	if ($_COOKIE["selected"] >= 1 && $_COOKIE["selected"] <= 6) {
 
-	$consulta='SELECT nombreProfesor,diaSemana,hora FROM vAusencias where hora='.$hora_actual;
-	verconsulta($conexion,$consulta);
-	
+		$consulta = 'SELECT nombreProfesor,diaSemana,hora FROM vAusencias where hora=' . $_COOKIE["selected"];
+
+		verconsulta($conexion, $consulta);
+	} else {
+
+		$hora_actual = saber_hora($hora, $minutos);
+
+		$consulta = 'SELECT nombreProfesor,diaSemana,hora FROM vAusencias where hora=' . $hora_actual;
+		verconsulta($conexion, $consulta);
+
 	}
 
 	print '<br/><br/><hr/><br/>';
-	
-	if(isset($_GET['profesor'])){
-		print '	<a href="index.php">Inicio</a>';	
+
+	if (isset($_GET['profesor'])) {
+		print '	<a href="index.php">Inicio</a>';
 	}
 
 	print '<h1 style="font-size:35px;">Otras ausencias</h1>';
-	
-		for($x=1;$x<=6;$x++){
-		if($x!=$hora_actual){
-		$consulta='SELECT nombreProfesor,diaSemana,hora FROM vAusencias where hora='.$x;
-	verconsulta($conexion,$consulta);
+
+	for ($x = 1; $x <= 6; $x++) {
+		if ($x != $hora_actual) {
+			$consulta = 'SELECT nombreProfesor,diaSemana,hora FROM vAusencias where hora=' . $x;
+			verconsulta($conexion, $consulta);
 		}
 	}
 	print '<br/><br/>';
-}	
+}
 ?>
 
 </div>
